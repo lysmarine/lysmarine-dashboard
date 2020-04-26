cb514387028c2e981c92489870b13b6717709761
Frederic Guilbault <2@0464.ca>
2020-04-26 11:03:42 -0400

    Add dynamic domain name

--- public/js.js
+++ public/js.js
@@ -1,25 +1,24 @@
 jQuery(document).ready(function(){
+    //On load, open the menu to begin.
     jQuery('.menu-elem-container').slideDown('slow')
+
+    // On init, set the base url of the iframe src
+    jQuery('iframe').each( (i,e)=>{
+        let url = jQuery(e).attr('src')
+        jQuery(e).attr('src','//'+window.location.hostname+url)
+    })
 
     //Open and close the menu from the handle.
     jQuery('.menuHandle').on('click',function(e){
         jQuery('.menuContainer').slideToggle('fast');
-        // lnk = jQuery(this).attr('iloc');
-        // console.log(lnk);
-
-        // jQuery('iframe').attr('src',lnk)
     });
 
+    // When click on the menu element, close the menu and reload iframe.
     jQuery('.menu-elem').on('click',function(e){
-
-        jQuery('.menuContainer').slideUp('slow');
-
         let target = jQuery(this).attr('id').replace('btn-','frame-');
-        console.log(jQuery(this).attr('id'))
-console.log(target)
+        jQuery('.menuContainer').slideUp('slow');
         jQuery('iframe').hide();
         jQuery('#'+target).show();
-
-
     });
+
 });

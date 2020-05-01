db048ab8ee34a969c582d9c9241b1aeeafe731d5
Frederic Guilbault <2@0464.ca>
2020-04-30 23:54:16 -0400

    Add bootstrap

--- public/js.js
+++ public/js.js
@@ -8,15 +8,17 @@ jQuery(document).ready(function(){
         jQuery(e).attr('src','//'+window.location.hostname+url)
     })
 
-    //Open and close the menu from the handle.
+    //Open and close the menu from the handle (both top and bottom).
     jQuery('.menuHandle').on('click',function(e){
         jQuery('.menuContainer').slideToggle('fast');
+        jQuery('div.menu-footer').toggle();
     });
 
     // When click on the menu element, close the menu and reload iframe.
     jQuery('.menu-elem').on('click',function(e){
         let target = jQuery(this).attr('id').replace('btn-','frame-');
-        jQuery('.menuContainer').slideUp('slow');
+        jQuery('.menuContainer').slideUp('fast');
+        jQuery('div.menu-footer').hide();
         jQuery('iframe').hide();
         jQuery('#'+target).show();
     });

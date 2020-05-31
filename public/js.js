205642d4277ffb06b4b31301446f5227cb20c3ae
Frederic Guilbault <2@0464.ca>
2020-05-31 17:27:39 -0400

    Avoid pre-load of iframes

--- public/js.js
+++ public/js.js
@@ -1,26 +1,20 @@
+
 jQuery(document).ready(function(){
-    //On load, open the menu to begin.
-    jQuery('.menu-elem-container').slideDown('slow')
 
-    // On init, set the base url of the iframe src
-    jQuery('iframe').each( (i,e)=>{
-        let url = jQuery(e).attr('src')
-        jQuery(e).attr('src','//'+window.location.hostname+url)
-    })
-
-    //Open and close the menu from the handle (both top and bottom).
     jQuery('.menuHandle').on('click',function(e){
-        jQuery('.menuContainer').slideToggle('fast');
-        jQuery('div.menu-footer').toggle();
+        jQuery('.menuContainer').slideDown('fast');
+        jQuery('iframe').hide()
     });
 
-    // When click on the menu element, close the menu and reload iframe.
     jQuery('.menu-elem').on('click',function(e){
-        let target = jQuery(this).attr('id').replace('btn-','frame-');
-        jQuery('.menuContainer').slideUp('fast');
-        jQuery('div.menu-footer').hide();
-        jQuery('iframe').hide();
-        jQuery('#'+target).show();
+        let frameID  = jQuery(this).attr('frameId')
+        jQuery('.menuContainer').hide( "puff" )
+
+        if(jQuery('#'+frameID).attr('src') ==='' ){
+            let frameUrl = jQuery(this).attr('frameUrl')
+            jQuery('#'+frameID).prop('src','http://'+window.location.hostname+frameUrl)
+        }
+        jQuery('#'+frameID).show();
     });
 
 });
